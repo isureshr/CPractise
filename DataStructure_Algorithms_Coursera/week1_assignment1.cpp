be407adbd5fb28faed0e4f30b2dbbd98912f9199
Suresh Ramamurthy <rsuresh1104@gmail.com>
2018-05-29 19:58:47 +0530

    week1 assignment1

---
+++ DataStructure_Algorithms_Coursera/week1_assignment1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a = 0, b = 0;
+	cout << "Enter two integers and result will be displayed" << endl;
+	cout << "Enter first number: ";
+	cin >> a;
+	cout << "Enter second number: ";
+	cin >> b;
+	cout << "The sum of two numbers = " << a+b << endl; 
+	return 1;
+}
